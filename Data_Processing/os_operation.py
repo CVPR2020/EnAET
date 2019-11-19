c2c1363da6f34cb16bc80fc3f4b47ee744af050f
CVPR2020 <EnAET@CVPR2020.com>
2019-11-19 11:28:55 +0800

    release all codes

---
+++ Data_Processing/os_operation.py
@@ -0,0 +1,20 @@
+# /*******************************************************************************
+# *  Author : CVPR2020_EnAET
+# *******************************************************************************/
+import os
+def mkdir(path):
+    path=path.strip()
+    path=path.rstrip("\\")
+    isExists=os.path.exists(path)
+    if not isExists:
+        print (path+" created")
+        os.makedirs(path)
+        return True
+    else:
+        print (path+' existed')
+        return False
+def execCmd(cmd):
+    r = os.popen(cmd)
+    text = r.read()
+    r.close()
+    return text
